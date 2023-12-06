e1d877fe0c75a9b0433892efb536cd456cf2e15b
Dean462 <153074003+Dean462@users.noreply.github.com>
2023-12-06 10:37:53 +0000

    datatypes

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,27 @@
+"use strict" // treat all JS code as newer version
+
+// alert(4+2) // we are using nodejs, not browser
+
+console.log(3
+    +
+    3)  // code readability shold be high
+
+console.log("dean")
+
+let name = "dean"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// bolean = true/false
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
